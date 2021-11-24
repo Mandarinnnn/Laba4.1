dd39d105b828f30ec442893eb3be66adbeff5085
almazislanov <almazislanovvv@yandex.ru>
2021-11-24 19:04:32 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Laba4
+{
+    class Shape
+    {
+        virtual public void draw()
+        {
+        }
+        virtual public bool isChecked(MouseEventArgs e)
+        {
+            return true;
+        }
+        virtual public void CheckedTrue()
+        {
+        }
+        virtual public void CheckedFalse()
+        {
+        }
+        virtual public void Draw(PictureBox pictureBox1, Graphics g, Bitmap bmp)
+        {
+        }
+    }
+
+    class Circle: Shape
+    {
+        private int x;
+        private int y;
+        private int r;
+        private bool Checked;
+        public Circle(int x,int y, int r)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            Checked = true;
+        }
+        public void Draw()
+        {
+
+        }
+        override public bool isChecked(MouseEventArgs e)
+        {
+            if (((e.X - x)*(e.X - x) + (e.Y - y) * (e.Y - y)) <= r * r)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        override public void CheckedTrue()
+        {
+            Checked = true;
+        }
+        override public void CheckedFalse()
+        {
+            Checked = false;
+        }
+
+        override public void Draw(PictureBox pictureBox1, Graphics g, Bitmap bmp)
+        {
+            Rectangle rect = new Rectangle(x - r, y - r, r * 2, r * 2);
+            Pen pen;
+            if (Checked== true)
+            {
+                pen = new Pen(Color.Blue);
+            }
+            else
+            {
+                pen = new Pen(Color.Black);
+            }
+            g.DrawEllipse(pen, rect);
+            pictureBox1.Image = bmp;
+        }
+    }
+
+    class MyStorage
+    {
+        private int size;
+        Shape[] storage;
+        public MyStorage()
+        {
+            size = 0;
+        }
+        public MyStorage(int size)
+        {
+            this.size = size;
+            storage = new Shape[size];
+        }
+
+        public void setObject(int i, Shape obj)
+        {
+            storage[i] = obj;
+        }
+
+        public void addObject(Shape obj)
+        {
+            Shape[] storage2 = new Shape [size+1];
+            if (size != 0)
+            {
+                for(int i = 0; i < size; i++)
+                {
+                    storage2[i] = storage[i];
+                }
+            }
+            storage = storage2;
+            storage[size] = obj;
+            size = size + 1;
+        }
+
+        public int getCount()
+        {
+            return size;
+        }
+
+        public bool isCheckedStorage(MouseEventArgs e)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if (storage[i].isChecked(e) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CheckedObjectStorage(MouseEventArgs e)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (storage[i].isChecked(e) == true)
+                {
+                    storage[i].CheckedTrue();
+                    break;
+                }
+            }
+        }
+
+        public void removeObject(int i, Shape obj)
+        {
+            if (size != 0 && i<size && size!=0)
+            {
+                Shape[] storage2 = new Shape[size - 1];
+                for (int j = 0; j < i; j++)
+                {
+                    storage2[j] = storage[j];
+                }
+                for (int j = i; j < size - 1; j++)
+                {
+                    storage2[j] = storage[j+1];
+                }
+                size = size - 1;
+                storage = storage2;
+            }
+            else if (size == 1)
+            {
+                storage[0] = null;
+                size = 0;
+            }
+        }
+
+        public void AllNotChecked()
+        {
+            for(int i = 0; i < size; i++)
+            {
+                storage[i].CheckedFalse();
+            }
+        }
+
+        public void DrawAll(PictureBox pictureBox1, Graphics g, Bitmap bmp)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                storage[i].Draw(pictureBox1, g, bmp);
+            }
+        }
+
+    }
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp = new Bitmap(1000, 1000);
+        Rectangle rect;
+        MyStorage storage = new MyStorage();
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(storage.isCheckedStorage(e)==false)//если нажато на пустое место
+            {
+                storage.AllNotChecked();
+                storage.addObject(new Circle(e.X,e.Y,30));
+                rect = new Rectangle(e.X - 32, e.Y - 32, 64, 64);
+            }
+            else//если нажать на круг
+            {
+                if(Control.ModifierKeys == Keys.Control)//если ctrl нажат
+                {
+                    storage.CheckedObjectStorage(e);
+                }
+                else//если ctrl не нажат
+                {
+                    storage.AllNotChecked();
+                    storage.CheckedObjectStorage(e);
+                }
+            }
+            this.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+
+            storage.DrawAll(pictureBox1, g, bmp);
+        }
+
+    }
+}
